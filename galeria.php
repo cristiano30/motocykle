c46d084b3d17de82ccd51b6d5d152a1fdca7cb29
cristiano30 <kaszuba93@gmail.com>
2018-03-21 20:48:31 +0100

    fii

---
+++ galeria.php
@@ -0,0 +1,18 @@
+ <?php include 'include/header.php'; ?>
+<div class="wide breadcrumbs ">
+	<div class="container ">
+		 <div class="row ">
+		    <ul class="breadcrumb   ">
+
+		      <li > Galeria  </li>
+		       
+		       
+		    </ul>
+		</div>
+	</div>
+</div>
+
+ <?php include 'include/pagesGaleria.php'; ?>
+
+ <?php include 'include/footer.php'; ?>
+ <?php include 'include/footer-scripts.php'; ?>
